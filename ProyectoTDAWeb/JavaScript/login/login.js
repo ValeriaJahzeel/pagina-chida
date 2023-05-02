024de39d1d49880aa0c40f5cf3a5894a738973e7
Navil <navyadenip@gmail.com>
2023-05-02 03:28:42 -0600

    Commit

--- ProyectoTDAWeb/JavaScript/login/login.js
+++ ProyectoTDAWeb/JavaScript/login/login.js
@@ -1,7 +1,11 @@
 function submitForm(event) {
     event.preventDefault();
+
     const user = document.getElementById('user-name');
     const usuario = user.value;
     console.log(usuario);
+
     window.location.href = "./index.html?id" + usuario;
+
+    user.value = '';
 }
