862820a169b23cbc1cd36c51509b0da6d2f62d10
Navil <navyadenip@gmail.com>
2023-05-01 23:44:39 -0600

    Commit

--- ProyectoTDAWeb/JavaScript/navegacion.js
+++ ProyectoTDAWeb/JavaScript/navegacion.js
@@ -1,21 +1,21 @@
 /* Barra de navegacion */
 function openNav() {
-     document.getElementById("mySidepanel").style.width = "250px";
-   }
-   
-   function closeNav() {
-     document.getElementById("mySidepanel").style.width = "0";
-   }
+  document.getElementById("mySidepanel").style.width = "250px";
+}
 
- 
-   
-   $(window).scroll(function(){
-       if ($(window).scrollTop() >= 300) {
-           $('nav').addClass('fixed-header');
-           $('nav div').addClass('visible-title');
-       }
-       else {
-           $('nav').removeClass('fixed-header');
-           $('nav div').removeClass('visible-title');
-       }
-   });
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
+
+
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() >= 300) {
+    $('nav').addClass('fixed-header');
+    $('nav div').addClass('visible-title');
+  }
+  else {
+    $('nav').removeClass('fixed-header');
+    $('nav div').removeClass('visible-title');
+  }
+});
